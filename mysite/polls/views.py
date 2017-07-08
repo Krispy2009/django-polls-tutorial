c46b88b80723ae8c2d08e14a41b0be3f544d9880
Kristi Achilleos <Krispy2009@gmail.com>
2017-07-08 16:04:38 +0100

    tutorial part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,27 +1,48 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.db.models import F
+from django.views import generic
+
+from .models import Question, Choice
 
 
-from .models import Question
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_questions'
 
-def index(request):
-
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    ctx = {'latest_questions' : latest_questions}
-
-    return render(request, 'polls/index.html', ctx)
-
-
-def detail(request, question_id):
-
-	question = get_object_or_404(Question, pk=question_id)
-
-	return render(request, 'polls/detail.html', {'question': question})
+	def get_queryset(self):
+		""" Return the last five published questions. """
+		return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-	return HttpResponse("You're looking at the results of question %s." % question_id)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+
+	question = get_object_or_404(Question, pk=question_id)
+
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except(KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': 'You did not select a message'
+			})
+	else:
+		selected_choice.votes = F('votes') + 1
+		selected_choice.save()
+
+		# Returning HttpResponseRedirect prevents data from being posted
+		# twice if a user hits the back button
+
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
